1f71e2a500eadf282a47437a956c0d5e406e508e
Devendra Singh <devsinghindra@gmail.com>
2020-04-19 23:45:42 +0530

    added procfile, changed color

--- app.js
+++ app.js
@@ -139,6 +139,11 @@ app.get("/:customListName", function (req, res) {
 
 })
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function () {
     console.log("Server is listening at port 3000");
 });
